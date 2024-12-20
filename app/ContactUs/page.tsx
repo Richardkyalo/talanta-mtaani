f5c6fff999c7266006158edcc7ee8fc8ffe0f218
RichardKyalo <richardkyalo57@gmail.com>
2024-12-20 16:37:29 +0300

    contact

---
+++ app/ContactUs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Contact from '../../components/Contact/Contact'
+// import Blog from '../../components/Blog/Blog'
+import "../../styles/globals.css"
+export default function Teams() {
+    return (
+    <div>
+        <Contact />
+    </div>
+);
+}
